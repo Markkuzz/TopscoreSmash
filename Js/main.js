86b00f7d3545bdce1d70e41ddc3454ac4697aa21
Markkuzz <mboncato21@gmail.com>
2022-06-23 13:52:35 +1000

    Mobile format almost complete, worked on popup

---
+++ Js/main.js
@@ -0,0 +1,50 @@
+// HAMBURGER MENU
+const hamBurger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo-container');
+
+hamBurger.addEventListener('click', ()=>{
+    hamBurger.classList.toggle('active');
+    nav.classList.toggle('active');
+    body.classList.toggle('active');
+    logo.classList.toggle('active')
+})
+
+// POPUP
+
+const openButton = document.querySelectorAll('[data-popup-target]');
+const closeButton = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+function popupFunction(){
+    openButton.forEach(div =>{
+        div.addEventListener('click', ()=>{
+            const popup = document.querySelector(div.dataset.popupTarget)
+            openPopup(popup)
+        })
+    })
+    overlay.addEventListener('click',()=>{
+        const popup = document.querySelectorAll('.popup.active');
+        popup.forEach(popup=>{
+            closePopup(popup)
+        })
+    } )
+    closeButton.forEach(div =>{
+        div.addEventListener('click', ()=>{
+            const popup = div.closest('.popup')
+            closePopup(popup)
+        })
+    })
+    
+    function openPopup(popup){
+        if (popup == null) return
+        popup.classList.add('active');
+        overlay.classList.add('active')
+    }
+    function closePopup(popup){
+        if (popup == null) return
+        popup.classList.remove('active');
+        overlay.classList.remove('active')
+    }
+}
